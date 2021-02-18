9fe6f5671c131aad53e7267b54748bdc38260ada
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-18 22:08:08 +0100

    Update test_exercice2.py

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,4 +1,5 @@
 from exercices.exercice2 import *
 
-def test_tri_iteratif():
-    assert tri_iteratif([41, 55, 21, 18, 12, 6, 25]) == [6, 12, 18, 21, 25, 41, 55]
+def test_rendu_glouton():
+    assert rendu_glouton_r(68,[],0) == [50, 10, 5, 2, 1]
+    assert rendu_glouton_r(291,[],0) == [100, 100, 50, 20, 20, 1]
