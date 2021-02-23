54e0fc84c3e65e85f79b35b23d5c4262d590f193
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-23 09:56:29 +0100

    modif nom de la fonction

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,5 +1,5 @@
 from exercices.exercice2 import *
 
 def test_rendu_glouton():
-    assert rendu_glouton_r(68,[],0) == [50, 10, 5, 2, 1]
-    assert rendu_glouton_r(291,[],0) == [100, 100, 50, 20, 20, 1]
+    assert rendu_glouton(68,[],0) == [50, 10, 5, 2, 1]
+    assert rendu_glouton(291,[],0) == [100, 100, 50, 20, 20, 1]
